dbbc3ab2ee9e0f080f9b30fdaa5bcd118d708444
johnlombardi389 <lombardij38@gmail.com>
2023-01-31 23:13:20 -0500

    Created form component

---
+++ src/features/posts/AddPostForm.js
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+import React from "react";
+
+const AddPostForm = () => {
+  return (
+    <div>
+      <h1>Form</h1>
+    </div>
+  );
+};
+
+export default AddPostForm;
