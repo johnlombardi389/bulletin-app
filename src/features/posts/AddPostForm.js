edb67162ecf10f3a85e498b71bf5855df42467fd
johnlombardi389 <lombardij38@gmail.com>
2023-02-01 11:31:34 -0500

    Added form to form component and created states to save user input

--- src/features/posts/AddPostForm.js
+++ src/features/posts/AddPostForm.js
@@ -1,12 +1,36 @@
 import { useState } from "react";
 
-import React from "react";
-
 const AddPostForm = () => {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  const onTitleChanged = (e) => setTitle(e.target.value);
+  const onContentChanged = (e) => setContent(e.target.value);
+
   return (
-    <div>
-      <h1>Form</h1>
-    </div>
+    <section>
+      <h2>Add a New Post</h2>
+      <form>
+        <label htmlFor="postTitle">Post Title:</label>
+        <input
+          type="text"
+          id="postTitle"
+          name="postTitle"
+          value={title}
+          onChange={onTitleChanged}
+        />
+
+        <label htmlFor="postContent">Content:</label>
+        <textarea
+          id="postContent"
+          name="postContent"
+          value={content}
+          onChange={onContentChanged}
+        />
+
+        <button type="button">Save Post</button>
+      </form>
+    </section>
   );
 };
 
