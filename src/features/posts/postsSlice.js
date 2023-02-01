a48dfada94f1e95083a5da44582185521caf4954
johnlombardi389 <lombardij38@gmail.com>
2023-01-31 22:41:53 -0500

    Created an initial state for posts

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,1 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = [
+  { id: 1, title: "Learning Redux Toolkit", content: "Is really fun." },
+  { id: 2, title: "Creating Slices", content: "This is my first slice." },
+];
