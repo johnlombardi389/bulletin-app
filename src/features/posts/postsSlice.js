34feeb7b6c5bc00776990baf0f52505f70915afb
johnlombardi389 <lombardij38@gmail.com>
2023-01-31 22:46:42 -0500

    Imported postsReducer to store

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -4,3 +4,13 @@ const initialState = [
   { id: 1, title: "Learning Redux Toolkit", content: "Is really fun." },
   { id: 2, title: "Creating Slices", content: "This is my first slice." },
 ];
+
+const postsSlice = (createSlice = {
+  name: "posts",
+  initialState,
+  reducers: [
+    // actions
+  ],
+});
+
+export default postsSlice.reducer;
