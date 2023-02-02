b60b16d08412de9cb335d4488aebb215056b1aca
johnlombardi389 <lombardij38@gmail.com>
2023-02-02 14:35:55 -0500

    Added userId prop to PostAuthor

--- src/features/posts/PostAuthor.js
+++ src/features/posts/PostAuthor.js
@@ -1,7 +1,7 @@
 import { useSelector } from "react-redux";
 import { selectAllUsers } from "../users/usersSlice";
 
-const PostAuthor = () => {
+const PostAuthor = ({ userId }) => {
   const users = useSelector(selectAllUsers);
 
   const author = users.find((user) => user.id === userId);
